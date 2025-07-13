26adbadcad51ffa3e73861307f8dbaa305151444
Arpit Mohapatra <arpitmohapatra06@gmail.com>
2025-07-13 11:11:17 +0530

    useFuture,useMemoized

--- learning_flutter_hooks/lib/main.dart
+++ learning_flutter_hooks/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform
+  ]) => 
+  map(
+    transform ?? (e) => e,
+  ).where((e)=> e!=null).cast();
+}
+
 void main() {
  runApp(
   const App()
@@ -24,31 +35,30 @@ class App extends StatelessWidget {
   }
 }
 
+const url = 'https://bit.ly/4kBPrj6';
+
 class HomePage extends HookWidget {
  const HomePage({super.key});
 
  @override
  Widget build(BuildContext context) {
-  final controller = useTextEditingController();
-  final text = useState('');
-  useEffect((){
-    controller.addListener((){
-      text.value = controller.text;
-    });
-    return null;
-  }, [controller]);
+  final future = useMemoized(
+    () =>
+    NetworkAssetBundle(
+    Uri.parse(url)
+    )
+    .load(url)
+    .then((data)=>data.buffer.asUint8List())
+    .then((data)=>Image.memory(data))
+  );
+  final snapshot = useFuture(future);
   return Scaffold(
    appBar: AppBar(
     title:Text('Home Page')
     ),
-    body:Column(
-        children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed ${text.value}'),
-        ]
-      ),
+    body: Column(
+      children: <Widget>[?snapshot.data].compactMap().toList(),
+    ),
   );
  }
 }
