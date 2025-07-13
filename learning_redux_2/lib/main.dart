d0dede7bd5d859be93d9e9b6d0e62ff3871da3cf
Arpit Mohapatra <arpitmohapatra06@gmail.com>
2025-07-13 20:40:27 +0530

    middleware

---
+++ learning_redux_2/lib/main.dart
@@ -0,0 +1,180 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
+void main() {
+ runApp(
+  const App()
+ );
+}
+const apiUrl = 'http://10.0.2.2:5500/api/people.json';
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+@immutable
+class Person{
+  final String name;
+  final int age;
+
+  const Person({
+    required this.name,
+    required this.age,
+  });
+
+  Person.fromJson(Map<String, dynamic> json)
+    : name = json['name'] as String,
+      age = json['age'] as int;
+
+  @override
+  String toString() => 'Person($name,$age years old)';
+}
+
+Future<Iterable<Person>> getPerson()=> 
+    HttpClient()
+        .getUrl(Uri.parse(apiUrl))
+        .then((req) => req.close())
+        .then((resp) => resp.transform(utf8.decoder).join())
+        .then((str) => json.decode(str) as List<dynamic>)
+        .then((list)=> list.map((e)=> Person.fromJson(e)));
+
+@immutable
+abstract class Action {
+  const Action();
+}
+
+@immutable
+class LoadPeopleAction extends Action {
+  const LoadPeopleAction();
+}
+
+@immutable
+class SuccessfullyFetchedPeopleAction extends Action{
+  final Iterable<Person> persons;
+  const SuccessfullyFetchedPeopleAction({required this.persons});
+}
+
+@immutable
+class FailedToFetchPeopleAction extends Action {
+  final Object error;
+  const FailedToFetchPeopleAction({required this.error});
+}
+
+@immutable
+class State{
+  final bool isLoading;
+  final Iterable<Person>? fetchedPersons;
+  final Object? error;
+
+  const State({required this.isLoading, required this.fetchedPersons, required this.error});
+  
+  const State.empty():
+    isLoading = false,
+    fetchedPersons = null,
+    error = null;
+}
+
+State reducer(State oldState, action){
+  if(action is LoadPeopleAction){
+    return const State(isLoading: true, fetchedPersons: null, error: null);
+  }
+  else if(action is SuccessfullyFetchedPeopleAction){
+    return State(isLoading: false, fetchedPersons: action.persons, error: null);
+  }
+  else if(action is FailedToFetchPeopleAction){
+    return State(isLoading: false, fetchedPersons: oldState.fetchedPersons, error: action.error);
+  }
+  
+  return oldState;
+  
+}
+
+void loadPeopleMiddleware(Store<State> store, action, NextDispatcher next){
+  
+  
+  if(action is LoadPeopleAction){
+    getPerson().then((persons){
+      store.dispatch(SuccessfullyFetchedPeopleAction(persons: persons));
+    }).catchError((error){
+      store.dispatch(FailedToFetchPeopleAction(error: error));
+    });
+  }
+  next(action);
+}
+
+
+
+class HomePage extends StatelessWidget {
+ const HomePage({super.key});
+
+ @override
+ Widget build(BuildContext context) {
+  final store = Store(reducer, initialState: const State.empty(), middleware: [loadPeopleMiddleware]);
+  return Scaffold(
+   appBar: AppBar(
+    title: const Text('Home Page'),
+   ),
+   body: StoreProvider(
+    store: store, 
+    child: Column(
+      children: [
+        TextButton(
+          onPressed: (){
+            store.dispatch(const LoadPeopleAction());
+          }, 
+          child: const Text('Load Persons')
+        ),
+        StoreConnector<State,bool>(
+          converter: (store) => store.state.isLoading,
+          builder: (context,isLoading){
+            if(isLoading){
+              return const CircularProgressIndicator();
+            }
+            else{
+              return const SizedBox();
+            }
+          }
+        ),
+        StoreConnector<State,Iterable<Person>?>(
+          converter: (store) => store.state.fetchedPersons,
+          builder: (context, people) {
+            if(people == null){
+              return const SizedBox();
+            }
+            return Expanded(
+              child: ListView.builder(
+                itemCount: people.length,
+                itemBuilder: (context,index){
+                  final person = people.elementAt(index);
+                  return ListTile(
+                    title: Text(person.name),
+                    subtitle: Text('${person.age} years old'),
+                  );
+                }
+                )
+              );
+          },
+        )
+      ],
+    )
+    )
+  );
+ }
+}
