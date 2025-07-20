485f11a226717c1be89f1d03a987747c4270beed
Arpit Mohapatra <arpitmohapatra06@gmail.com>
2025-07-20 12:51:32 +0530

    FutureProvider,StateProvider,AsyncValue

--- riverpod_learning/lib/main.dart
+++ riverpod_learning/lib/main.dart
@@ -7,21 +7,6 @@ void main() {
  );
 }
 
-extension OptionalInfixAddition<T extends num> on T? {
-  T? operator +(T? other) {
-    if (this == null && other == null) return null;
-    return (this ?? 0 as T) + (other ?? 0 as T) as T;
-  }
-}
-
-class Counter extends StateNotifier<int?> {
-  Counter() : super(null);
-  void increment() =>state = state == null ? 1 : state + 1;
-}
-
-final counterProvider = StateNotifierProvider<Counter, int?>(
-  (ref) => Counter()
-);
 
 
 class App extends StatelessWidget {
@@ -42,32 +27,77 @@ class App extends StatelessWidget {
   }
 }
 
+enum City {
+  waterloo,
+  stalingrad,
+  berlin,
+  paris,
+  auschwitz,
+}
 
+typedef WeatherEmojy = String;
+
+Future<WeatherEmojy> getWeather(City city){
+  // Simulate a network call
+  return Future.delayed(
+    const Duration(seconds: 1),
+    () =>{
+      City.waterloo: '🌧️',
+      City.stalingrad: '❄️',
+      City.berlin: '☀️',
+      City.paris: '🌤️',
+      City.auschwitz: '🌪️',
+    }[city]!,
+  );
+}
+
+const unknownWeatherEmojy = '🤷‍♂️';
+
+final currentCityProvider = StateProvider<City?>((ref)=>null,);
+final weatherProvider = FutureProvider((ref){
+  final city = ref.watch(currentCityProvider);
+  if (city == null) {
+    return unknownWeatherEmojy;
+  }
+  return getWeather(city);
+});
 
 class HomePage extends ConsumerWidget {
  const HomePage({super.key});
 
  @override
  Widget build(BuildContext context, WidgetRef ref) {
+  final currentWeather = ref.watch(weatherProvider);
   return Scaffold(
    appBar: AppBar(
-    title: Consumer(
-      builder: (context, ref, child) {
-        final count = ref.watch(counterProvider);
-        final text = count == null ? 'Press the button' : count.toString();
-        return Text(text);
-      },
-    ),
+    title: const Text('Weather'),
    ),
    body: Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
     children: [
-      TextButton(
-        onPressed: () => ref.read(counterProvider.notifier).increment(), 
-        child: const Text('Increment Counter')
+      currentWeather.when(
+        data: (data) => Text(data as String, style: const TextStyle(fontSize: 40),),
+        loading: () => const CircularProgressIndicator(),
+        error: (_,__)=> const Text('FAT BOY 💣 DROPPED HERE!!')
+      ),
+      const SizedBox(height: 20),
+      Expanded(
+        child: ListView.builder(
+          itemCount: City.values.length,
+          itemBuilder: (context, index){
+            final city = City.values[index];
+            final isSelected = city == ref.watch(currentCityProvider);
+            return ListTile(
+              title: Text(city.name),
+              trailing: isSelected ? const Icon(Icons.check) : null,
+              onTap: (){
+                ref.read(currentCityProvider.notifier).state = city;
+              },
+            );
+          }
+        )
       )
-    ],
-   )
+    ]
+   ),
   );
  }
 }
