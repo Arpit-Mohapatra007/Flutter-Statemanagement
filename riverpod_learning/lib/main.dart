6cd93e0e332f4f2d5d3a341ceb3fe88bbb762dcd
Arpit Mohapatra <arpitmohapatra06@gmail.com>
2025-07-20 11:13:55 +0530

    StateNotifierProvider,ref.read()

--- riverpod_learning/lib/main.dart
+++ riverpod_learning/lib/main.dart
@@ -3,16 +3,31 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
  runApp(
-  ProviderScope(
-    child: const App()
-    )
+  ProviderScope(child: const App())
  );
 }
 
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    if (this == null && other == null) return null;
+    return (this ?? 0 as T) + (other ?? 0 as T) as T;
+  }
+}
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+  void increment() =>state = state == null ? 1 : state + 1;
+}
+
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter()
+);
+
+
 class App extends StatelessWidget {
   const App({
-    Key? key,
-  }) : super(key: key);
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -27,26 +42,32 @@ class App extends StatelessWidget {
   }
 }
 
-final currentDate = Provider<DateTime>(
-  (ref) => DateTime.now(),
-);
 
-class HomePage extends ConsumerWidget{
-  const HomePage ({super.key});
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final date = ref.watch(currentDate);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HOOKS RIVERPOD'),
-      ),
-      body: Center(
-        child: Text(
-          date.toIso8601String(),
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ),
-    );
-  }
+class HomePage extends ConsumerWidget {
+ const HomePage({super.key});
+
+ @override
+ Widget build(BuildContext context, WidgetRef ref) {
+  return Scaffold(
+   appBar: AppBar(
+    title: Consumer(
+      builder: (context, ref, child) {
+        final count = ref.watch(counterProvider);
+        final text = count == null ? 'Press the button' : count.toString();
+        return Text(text);
+      },
+    ),
+   ),
+   body: Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      TextButton(
+        onPressed: () => ref.read(counterProvider.notifier).increment(), 
+        child: const Text('Increment Counter')
+      )
+    ],
+   )
+  );
+ }
 }
